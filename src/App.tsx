9aa7dd2bce49abe71c3d13acebf08f537c04caad
Станислав Пшедзял <spshedzyal@it-fintech.com>
2023-12-18 13:56:25 +0000

    Add Notifications  in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "dayjs/locale/ru";
 import { SWRConfig } from "swr";
 import { RouterProvider } from "react-router-dom";
+import { Notifications } from "@mantine/notifications";
 import { MantineProvider } from "@mantine/core";
 import { DatesProvider } from "@mantine/dates";
 import { fetcher } from "@/shared/api";
@@ -15,6 +16,7 @@ export default function App() {
           {
                   <SWRConfig value={{ fetcher }}>
                       <MantineProvider defaultColorScheme="auto">
+                      <Notifications />
                           <DatesProvider settings={{ locale: "ru" }}>
                               <RouterProvider router={router} />
                           </DatesProvider>
